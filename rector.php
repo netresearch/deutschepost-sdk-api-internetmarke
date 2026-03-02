2361b00bd4937247fea6daf89f3f65f65420d6a2
Sebastian <sebastian.ertner@netresearch.de>
2026-03-02 11:38:31 +0100

    DHLGW-1487: SDK project setup

---
+++ rector.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
+use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/test',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_83)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        earlyReturn: true,
+        typeDeclarations: true,
+        privatization: true
+    )
+    ->withPhpSets()
+    ->withSets([
+        PHPUnitSetList::PHPUNIT_100
+    ])
+    ->withRules([
+        ExplicitNullableParamTypeRector::class,
+    ])
+    // Skip feature sets and rules that might cause issues
+    ->withSkip([
+        ReadOnlyPropertyRector::class,
+        ClassPropertyAssignToConstructorPromotionRector::class,
+        RemoveUnusedPrivatePropertyRector::class,
+        RemoveEmptyClassMethodRector::class,
+        RemoveUnusedPromotedPropertyRector::class
+    ]);
